9f761ec8969a63953411cc0626a3c51f73e79bfb
Dmitriy <>
2018-10-18 12:43:56 +0300

    min

---
+++ static_src/scripts/partials/min.js
@@ -0,0 +1,14 @@
+function min(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        if (a < b) {
+            console.log(a);
+        } else {
+            console.log(b);
+        }
+    } else {
+        console.log("не сравнить")
+    }
+}
+min(0, 10);
+min(5, 1);
+min('1', 0);
